e1fac230bf1953748de91a50a667641f21253470
illyakim <ilyakm93@gmail.com>
2023-08-04 21:48:27 +0600

    added rating function

--- server/Rates/Rates.js
+++ server/Rates/Rates.js
@@ -1,12 +1,15 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
-const GenreScheme = new mongoose.Schema({
+const RateScheme = new mongoose.Schema({
     rate: Number,
     text: String,
     filmId: { type: Schema.Types.ObjectId, ref: 'film' },
     authorId: { type: Schema.Types.ObjectId, ref: 'user' },
-    date: Date.now(),
+    date: {
+        type: Date,
+        default: Date.now()
+    }
 })
 
-module.exports = mongoose.model('genre', GenreScheme)
+module.exports = mongoose.model('rate', RateScheme)
