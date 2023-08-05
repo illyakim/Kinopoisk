5b5a51110cf53b8a22eeb250ae7eb05fe1a60eaa
illyakim <ilyakm93@gmail.com>
2023-08-05 16:22:17 +0600

    added google authorization

--- server/config/passport.js
+++ server/config/passport.js
@@ -2,6 +2,14 @@ const passport = require('passport')
 const User = require('../auth/User')
 const bcrypt = require('bcrypt')
 const LocalStrategy = require('passport-local')
+const GoogleStrategy = require('passport-google-oauth20').Strategy
+
+
+// CLIENT_ID
+// 1011746929413-ll0ggad44920f1a2ikf3nrt0pln9k18e.apps.googleusercontent.com
+// CLIENT_SECRET
+// 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
+
 
 passport.use(new LocalStrategy(
     {
@@ -21,6 +29,25 @@ passport.use(new LocalStrategy(
             return done(e)
         })
     }))
+
+passport.use(new GoogleStrategy({
+    clientID: '1011746929413-ll0ggad44920f1a2ikf3nrt0pln9k18e.apps.googleusercontent.com',
+    clientSecret: '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',
+    callbackURL: "http://localhost:8000/api/auth/google",
+    scope: ['openid', 'email', 'profile']
+},
+    async function (accessToken, refreshToken, profile, cb) {
+        const user = await User.find({ googleId: profile.id })
+        console.log(profile)
+        const newUser = await new User({
+            googleId: profile.id,
+            full_name: profile.displayName,
+            email: profile.emails[0].value
+        }).save()
+        return cb(null, newUser);
+    })
+)
+
 
 passport.serializeUser(function (user, done) {
     console.log(user);
